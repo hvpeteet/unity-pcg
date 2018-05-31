a7fd8c459ddf57a95963ecb90264494965e6f617
Henry Peteet <hvpeteet@gmail.com>
2018-05-31 14:24:56 -0400

    Cleaning up Utils
    Added a test for CalcCDF and added comments to the Utils.cs file. TODO: finish tests for all functions in Utils.cs

---
+++ BuildingGeneration/Assets/Editor/UtilsTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class UtilsTest {
+
+    public class doubleComparer : IComparer
+    {
+        private double tolerance;
+        public doubleComparer(double tolerance)
+        {
+            this.tolerance = tolerance;
+        }
+
+        // Calls CaseInsensitiveComparer.Compare with the parameters reversed.
+        int IComparer.Compare(System.Object x, System.Object y)
+        {
+            double diff = (double)x - (double)y;
+            if (diff < tolerance)
+            {
+                return 0;
+            } else if (diff < 0)
+            {
+                return -1;
+            } else
+            {
+                return 1;
+            }
+        }
+
+    }
+
+    [Test]
+	public void UtilsTestSimplePasses() {
+        // Use the Assert class to test conditions.
+        double[] cdf = Utils.CalcCDF(new int[] { 1, 3, 1, 5});
+        CollectionAssert.AreEqual(new double[] { 0.1, 0.4, 0.5, 1.0 }, cdf, new doubleComparer(0.0001));
+	}
+}
